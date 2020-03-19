51d2bc28546545cf0a8047427763bd1a3731a0e1
Tonalli <tonalli@xanum.uam.mx>
2020-03-19 17:25:01 -0600

    Add delete project-id route

--- routes/project-routes.js
+++ routes/project-routes.js
@@ -60,4 +60,18 @@ router.put('/projects/:id', (req, res, next) => {
     .catch(err => res.json(err));
 });
 
+// DELETE route => to delete a specific project
+router.delete('/projects/:id', (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Project.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.json({ message: `Project with ${req.params.id} is removed successfully.` });
+    })
+    .catch(err =>  res.json(err));
+});
+
 module.exports = router;
