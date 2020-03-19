921c135effed31d97b4c6a67be221ad070707240
Tonalli <tonalli@xanum.uam.mx>
2020-03-19 17:05:03 -0600

    Add get project-id route

--- routes/project-routes.js
+++ routes/project-routes.js
@@ -22,9 +22,26 @@ router.post('/projects', (req, res, next) => {
 // GET route => to get all the projects
 router.get('/projects', (req, res, next) => {
   Project.find()
-    .populate('task')
+    .populate('tasks')
     .then(response => {
       res.json(response); // all the projects
+    })
+    .catch(err => res.json(err));
+});
+
+// GET route => to get a specific project/detailed view
+router.get('/projects/:id', (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  // Our project have array of tasks' ids and
+  // we can use .populate() method to get the whole task objects
+  Project.findById(req.params.id)
+    .populate('tasks')
+    .then(response => { // a specific project
+      res.status(200).json(response);
     })
     .catch(err => res.json(err));
 });
