b05d4383c14922f5246ba48e7dc93fc7b52fbc4d
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-03-20 17:31:55 -0600

    Update port in projects_post.sh

--- scripts/projects_post.sh
+++ scripts/projects_post.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl --location --request POST 'localhost:3000/api/projects' \
+curl --location --request POST 'localhost:5000/api/projects' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'title=Learn React' \
 --data-urlencode 'description=Get React skills and build killing final project app'
