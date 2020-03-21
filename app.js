4e46c6b498c5ae983c58ddbcaea891dee589d54e
Tonalli <tonalli@xanum.uam.mx>
2020-03-21 14:22:28 -0600

    Add auth-routes

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ const cors         = require('cors');
 // INSTALL THESE DEPENDENCIES: passport-local, passport, bcryptjs, express-session
 // AND UN-COMMENT OUT FOLLOWING LINES:
 
-// const session       = require('express-session');
-// const passport      = require('passport');
+const session       = require('express-session');
+const passport      = require('passport');
 
-// require('./configs/passport');
+require('./configs/passport');
 
 // IF YOU STILL DIDN'T, GO TO 'configs/passport.js' AND UN-COMMENT OUT THE WHOLE FILE
 
@@ -44,9 +44,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ADD SESSION SETTINGS HERE:
+app.use(session({
+  secret: "some secret goes here",
+  resave: true,
+  saveUninitialized: true
+}));
 
 
 // USE passport.initialize() and passport.session() HERE:
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 // ADD CORS SETTINGS HERE TO ALLOW CROSS-ORIGIN INTERACTION:
